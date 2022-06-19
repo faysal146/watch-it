17592b57dca4ab6e3647d5ead94f92cb2996eacb
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-19 19:46:35 +0600

    feat: :lipstick: add windicss as ui style lib
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,28 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, watch, onMounted } from "vue";
+
+const isDark = ref(false);
+
+onMounted(() => {
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		isDark.value = true;
+	}
+});
+watch(isDark, () => {
+	if (isDark.value) document.documentElement.classList.add("dark");
+	else document.documentElement.classList.remove("dark");
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<div
+		class="w-full min-h-screen dark:(bg-dark-600 text-light-600) bg-light-600 text-dark-600"
+	>
+		<button
+			class="text-xl font-bold py-2 px-6 rounded-md shadow-light-400 dark:(bg-dark-800) bg-light-800"
+			@click="isDark = !isDark"
+		>
+			{{ isDark ? "Light" : "Dark" }}
+		</button>
+	</div>
 </template>
-
-<style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
