5617ac24d6d981d1061f6a82177f4ff5ead18df7
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-19 19:42:51 +0600

    docs: :technologist: add windicss ext as recommendations
    add windicss to the recommendations ext list.
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
+import WindiCSS from "vite-plugin-windicss";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [WindiCSS(), vue()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
