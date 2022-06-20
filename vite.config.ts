bdf6cfd924237e522815d4dfdd6e4a4d3bf93e64
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-21 01:42:51 +0600

    ci: :hammer: add auto import plugin to windicss
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { fileURLToPath, URL } from "url";
 import WindiCSS from "vite-plugin-windicss";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// import AutoImport from "unplugin-auto-import";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [WindiCSS(), vue()],
+	plugins: [
+		WindiCSS(),
+		vue(),
+		// AutoImport({
+		// 	imports: ["vue"],
+		// }),
+	],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
