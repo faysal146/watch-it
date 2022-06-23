dca01d3d114ab905488474533d44b1ed97f90c98
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-23 15:12:10 +0600

    feat: :sparkles: implement dark or light mode
    implement dark mode and light mode using vueuse #useDark
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from "vue";
-
-const isDark = ref(false);
-
-onMounted(() => {
-	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-		isDark.value = true;
-	}
-});
-watch(isDark, () => {
-	if (isDark.value) document.documentElement.classList.add("dark");
-	else document.documentElement.classList.remove("dark");
-});
+import { useDark, useToggle } from "@vueuse/core";
+import { provide } from "vue";
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+provide("toggleDark", toggleDark);
 </script>
 
 <template>
-	<div
-		class="w-full min-h-screen dark:(bg-dark-600 text-light-600) bg-light-600 text-dark-600"
-	>
-		<button
-			class="text-xl font-bold py-2 px-6 rounded-md shadow-light-400 dark:(bg-dark-800) bg-light-800"
-			@click="isDark = !isDark"
-		>
-			{{ isDark ? "Light" : "Dark" }}
-		</button>
-	</div>
+	<Router-View></Router-View>
 </template>
+
+<style>
+@import url("./assets/base.css");
+</style>
