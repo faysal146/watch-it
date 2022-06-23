0200f38eeba8bdb33d6eea9bc16f488116f51e41
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-23 15:34:02 +0600

    feat: add home page
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
+<template>
+	<WithSideBar>
+		<div></div>
+	</WithSideBar>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import WithSideBar from "@/layouts/WithSideBar.vue";
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
