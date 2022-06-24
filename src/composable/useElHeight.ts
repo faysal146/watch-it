51732534b78818ea844a2c53c63c797ee8ea5a69
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-24 20:48:45 +0600

    revert: :fire: get element current position
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/composable/useElHeight.ts
+++ src/composable/useElHeight.ts
@@ -2,17 +2,18 @@ import { useElementBounding } from "@vueuse/core";
 import { ref, watch } from "vue";
 
 const el = ref(null);
-const position = ref(0);
+const headerElePosition = ref(0);
 export default function useElHeight() {
-	const { height } = useElementBounding(el);
+	const { height, y: position } = useElementBounding(el);
+	// watch(top, console.log);
 	watch(el, e => {
 		if (e) {
 			const styles = window.getComputedStyle(e);
 			const margins =
 				parseInt(styles["marginBottom"].replace("px", "")) +
 				parseInt(styles["marginTop"].replace("px", ""));
-			position.value = height.value + margins + 10;
+			headerElePosition.value = height.value + margins;
 		}
 	});
-	return { el, height: position };
+	return { el, height: headerElePosition, position };
 }
