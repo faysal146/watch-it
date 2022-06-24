ffef346a76182779f48ad46d1e73064188d6fa9e
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-06-24 20:58:19 +0600

    feat: :construction: home page (hot finish yet)
    home page add header, sidebar and vidoe component (not finish yet)
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
-	<WithSideBar>
-		<div></div>
+	<WithSideBar v-slot="slotProps">
+		<section
+			:class="`app-bg-main ${slotProps.class} flex flex-wrap gap-4 p-3`"
+		>
+			<VideoCard
+				:thumb="faker.image.city()"
+				:title="faker.lorem.words(10)"
+				:length="200"
+				:author="{
+					username: 'Faysal Ahmed',
+					profileLink: '/faysal-ahmed',
+					profileAvater: faker.image.avatar(),
+				}"
+				:likeCount="20"
+				:views="229"
+			/>
+		</section>
 	</WithSideBar>
 </template>
 
 <script setup lang="ts">
+import { faker } from "@faker-js/faker";
 import WithSideBar from "@/layouts/WithSideBar.vue";
+import VideoCard from "../components/VideoCard.vue";
 </script>
 
 <style scoped></style>
