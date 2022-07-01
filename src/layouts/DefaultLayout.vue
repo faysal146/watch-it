8397535bd1fc3e3e3b3f3cc43d6d8adaee5c17b1
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-07-01 18:55:36 +0600

    fix: :fire: remove footer element
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import Footer from "@/components/AppFooter.vue";
 import Header from "@/components/AppHeader.vue";
 </script>
 
 <template>
 	<Header></Header>
 	<main><slot /></main>
-	<Footer></Footer>
 </template>
